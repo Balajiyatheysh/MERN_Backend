3ee4ee5316e41c57b2ed44facc1acba8aae39c0e
balajiyatheysh <balajiyatheysh87@gmail.com>
2023-11-11 12:43:58 +0530

    created utils for error handling

---
+++ src/index.js
@@ -0,0 +1,62 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import { app } from './app.js';
+// import express from 'express';
+// const app = express()
+
+dotenv.config({
+  path:'./env'
+})
+
+
+
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8001, ()=>{
+    console.log(`Server is running at port : ${process.env.PORT}`)
+  })
+  app.on("error",(error)=>{
+    console.log("Error: ", error);
+    throw error
+  })
+  app.get('/', (req, res)=>{
+    res.send("hurray")
+  })
+})
+.catch((error)=>{
+  console.log("MONGODB connection failed..!!", error)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongooes.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+})();
+*/
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+// const asyncHandler =()=>{}
+// const asyncHandler =(fn)=>()=>{}
+// const asyncHandler =(fn)=>async()=>{}
+
+
+
+
+// const asyncHandler = (fn) = async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       sucess: false,
+//       message: error.message,
+//     });
+//   };
+// };
+
+const asyncHandler = (requestHandler)=>{
+  (req, res, next)=>{
+    Promise.resolve(requestHandler(req, res, next)).catch((error)=>{
+      next(error);
+    });
+  };
+};
+
+export {asyncHandler}
