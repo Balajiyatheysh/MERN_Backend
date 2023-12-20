8f128866a8a967d0aaa9dc4c3e4247fe9a8b4c0b
balajiyatheysh <balajiyatheysh87@gmail.com>
2023-12-20 14:35:02 +0530

    routes and middlewares

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from 'jsonwebtoken';
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+try {
+  const token = req.cookies?.accesstoken || req.header("Authorization").replace("Bearer ", "");
+  console.log(token)
+
+  if (!token) {
+    throw new ApiError(401, "Not authorized to access this route");
+  }
+
+  const decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  const user = await User.findById(decodeToken?._id).select("-password -refreshtoken");
+
+  if (!user) {
+    // discuss about frontend
+    throw new ApiError(401, "Invalid accessToken");
+  }
+
+  req.user = user;
+  next();
+} catch (error) {
+  throw new ApiError(401, error?.message || "Invalid access token");
+}
+})
